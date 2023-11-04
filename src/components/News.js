950ad2528b39edf38b2f7f484d58e7f934de4f6e
KunalBhika <kunalbhika2@gmail.com>
2023-11-04 20:54:12 +0530

    Populated data using fetch api

--- src/components/News.js
+++ src/components/News.js
@@ -9,7 +9,16 @@ export class News extends Component {
             articles : data.articles
         }
     }
+
+    async componentDidMount() {
+        let url = 'https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=9180273d9b564bb3b4b437b702785a4a';
+        let data = await fetch(url);
+        let parsedData = await data.json();
+        this.setState({articles : parsedData.articles});
+    }
+
     render() {
+        let defaultImage = "https://media.istockphoto.com/id/1311148884/vector/abstract-globe-background.jpg?s=2048x2048&w=is&k=20&c=ZyHCcX0F_DVM-r_R_vG8OX_CqYLb-G16afTyaVGtB3o=";
         return (
             <>
                 <div className="container my-3">
@@ -19,7 +28,7 @@ export class News extends Component {
                         {
                             this.state.articles.map((element) => (
                                 <div className="col-md-4" key={element.url}>
-                                    <NewsItem title={element.title} imgUrl={element.urlToImage} url={element.url}/>
+                                    <NewsItem title={element.title.slice(0,50)} desc={element.description?element.description.slice(0,100):" "} imgUrl={element.urlToImage?element.urlToImage:defaultImage} url={element.url}/>
                                 </div>
                             ))
                         }
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -2,18 +2,18 @@ import React, { Component } from 'react'
 
 export class NewItem extends Component {
   render() {
-    let {title, imgUrl, url} = this.props;
+    let {title, desc, imgUrl, url} = this.props;
     return (
       <div className="card my-4" style={{width: "18rem"}}>
         <img className="card-img-top" src={imgUrl} alt="Card image cap" />
         <div className="card-body">
           <h5 className="card-title">{title}</h5>
-          <p className="card-text"></p>
+          <p className="card-text">{desc}</p>
           <a href={url} className="btn btn-sm btn-primary" target="_blank">Read more..</a>
         </div>
       </div>
     )
-  }
+  } 
 }
 
 export default NewItem;
