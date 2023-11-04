da7fe006abddc1d50e03dca4e7e0cdf9240d366d
KunalBhika <kunalbhika2@gmail.com>
2023-11-04 23:43:06 +0530

    made prev and next button functionality - still need to improved

--- src/components/News.js
+++ src/components/News.js
@@ -6,12 +6,31 @@ export class News extends Component {
     constructor() {
         super();
         this.state = {
-            articles : []
+            articles : [],
+            page : 1
         }
     }
 
     async componentDidMount() {
-        let url = 'https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=9180273d9b564bb3b4b437b702785a4a';
+        let url = 'https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=9180273d9b564bb3b4b437b702785a4a&page=1';
+        let data = await fetch(url);
+        let parsedData = await data.json();
+        this.setState({articles : parsedData.articles});
+    }
+
+    onNextBtn = async () => {
+        const nextPage = this.state.page + 1;
+        this.setState({page : nextPage});
+        let url = `https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=9180273d9b564bb3b4b437b702785a4a&page=${nextPage}`;
+        let data = await fetch(url);
+        let parsedData = await data.json();
+        this.setState({articles : parsedData.articles});
+    }
+
+    onPrevBtn = async () => {
+        const prevPage = this.state.page - 1;
+        this.setState({page : prevPage});
+        let url = `https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=9180273d9b564bb3b4b437b702785a4a&page=${prevPage}`;
         let data = await fetch(url);
         let parsedData = await data.json();
         this.setState({articles : parsedData.articles});
@@ -19,6 +38,7 @@ export class News extends Component {
 
     render() {
         let defaultImage = "https://media.istockphoto.com/id/1311148884/vector/abstract-globe-background.jpg?s=2048x2048&w=is&k=20&c=ZyHCcX0F_DVM-r_R_vG8OX_CqYLb-G16afTyaVGtB3o=";
+
         return (
             <>
                 <div className="container my-3">
@@ -28,12 +48,16 @@ export class News extends Component {
                         {
                             this.state.articles.map((element) => (
                                 <div className="col-md-4" key={element.url}>
-                                    <NewsItem title={element.title.slice(0,50)} desc={element.description?element.description.slice(0,100):" "} imgUrl={element.urlToImage?element.urlToImage:defaultImage} url={element.url}/>
+                                    <NewsItem title={element.title} desc={element.description?element.description.slice(0,100):" "} imgUrl={element.urlToImage?element.urlToImage:defaultImage} url={element.url}/>
                                 </div>
                             ))
                         }
                         
                     </div>
+                </div>
+                <div className="container my-5">
+                    <button id="prev-btn" className="btn btn-success mx-2" onClick={this.onPrevBtn} disabled={this.state.page <= 1}>Prev</button>
+                    <button id="next-btn" className="btn btn-success mx-2" onClick={this.onNextBtn}>Next</button>
                 </div>
             </>
         )
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -8,7 +8,7 @@ export class NewItem extends Component {
         <img className="card-img-top" src={imgUrl} alt="Card image cap" />
         <div className="card-body">
           <h5 className="card-title">{title}</h5>
-          <p className="card-text">{desc}</p>
+          <p className="card-text">{desc}..</p>
           <a href={url} className="btn btn-sm btn-primary" target="_blank">Read more..</a>
         </div>
       </div>
