3fce37ed35065bbbcb6e68a09cc3e5501ecbc469
KunalBhika <kunalbhika2@gmail.com>
2023-11-04 20:57:00 +0530

    Update News.js

--- src/components/News.js
+++ src/components/News.js
@@ -6,7 +6,7 @@ export class News extends Component {
     constructor() {
         super();
         this.state = {
-            articles : data.articles
+            articles : []
         }
     }
 
