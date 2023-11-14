26dece8ad2148b72797f2dbfbc942db11e20da98
KunalBhika <kunalbhika2@gmail.com>
2023-11-15 01:09:25 +0530

    Added infinite scroll and commented code for next and previous button

--- src/components/News.js
+++ src/components/News.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react'
 import NewsItem from './NewsItem'
 import Spinner from './Spinner';
+import InfiniteScroll from 'react-infinite-scroll-component';
 
 export class News extends Component {
-    constructor() {
-        super();
+    constructor(props) {
+        super(props);
         this.state = {
             articles: [],
             page: 1,
@@ -12,6 +13,7 @@ export class News extends Component {
             totalResults: 0,
             isLoading: true
         }
+        document.title = "NewsZilla - " + this.props.category;
     }
 
     async updateNews() {
@@ -19,7 +21,7 @@ export class News extends Component {
         const url = `https://newsapi.org/v2/top-headlines?country=in&category=${this.props.category}&apiKey=9180273d9b564bb3b4b437b702785a4a&page=${this.state.page}&pageSize=${this.props.pageSize}`;
         let data = await fetch(url);
         let parsedData = await data.json();
-        this.setState({ articles: parsedData.articles, totalResults: parsedData.totalResults, isLoading: false });
+        this.setState({ articles: this.state.articles.concat(parsedData.articles), totalResults: parsedData.totalResults, isLoading: false });
     }
 
     async componentDidMount() {
@@ -28,23 +30,39 @@ export class News extends Component {
 
     onNextBtn = async () => {
         this.setState({ page: this.state.page + 1 });
-        this.updateNews();
+        setTimeout(() => {    // Added this small delay to give some time to this.state.page to update.
+            this.updateNews();
+        }, 90)
     }
 
-    onPrevBtn = async () => {
-        this.setState({ page: this.state.page - 1 });
-        this.updateNews();
-    }
+    // onPrevBtn = async () => {
+    //     this.setState({ page: this.state.page - 1 });
+    //     setTimeout(() => {     // Added this small delay to give some time to this.state.page to update.
+    //         this.updateNews();
+    //     }, 90)
+    // }
 
     render() {
         const defaultImage = "https://media.istockphoto.com/id/1311148884/vector/abstract-globe-background.jpg?s=2048x2048&w=is&k=20&c=ZyHCcX0F_DVM-r_R_vG8OX_CqYLb-G16afTyaVGtB3o=";
         return (
             <>
                 <div className="container my-3">
-                    <h2 className="mainTitle text-center" style={{ margin: '40px 0px' }}>NewsZilla - One Stop for your daily news dose</h2>
+                    <h2 className="mainTitle text-center" style={{ margin: '40px 0px' }}>NewsZilla - top {this.props.category} headlines</h2>
 
                     {/* if page is loading then show spinner else show actual data */}
-                    {this.state.isLoading ? <Spinner /> :
+
+                    <InfiniteScroll
+                        style={{overflow : 'none'}}
+                        dataLength={this.state.articles.length} //This is important field to render the next data
+                        next={this.onNextBtn}
+                        hasMore={this.state.totalShown < this.state.totalResults}
+                        loader={<Spinner/>}
+                        endMessage={
+                            <p style={{ textAlign: 'center', fontSize : '14px', color: 'grey' }}>
+                                You have fulfilled your daily {this.props.category} news dose
+                            </p>
+                        }
+                    >
                         <div className="row">
                             {
                                 this.state.articles.map((element) => (
@@ -55,13 +73,14 @@ export class News extends Component {
                                 ))
                             }
                         </div>
-                    }
+                    </InfiniteScroll>
+
 
                 </div>
-                <div className="container my-5">
+                {/* <div className="container my-5">
                     <button id="prev-btn" className="btn btn-dark mx-2" onClick={this.onPrevBtn} disabled={this.state.page <= 1}>Prev</button>
                     <button id="next-btn" className="btn btn-dark mx-2" onClick={this.onNextBtn} disabled={this.state.totalShown >= this.state.totalResults}>Next</button>
-                </div>
+                </div> */}
             </>
         )
     }
